29abaf55795297f8e32b1393514451916539b5bd
janani082004 <jj961954@gmail.com>
2024-12-12 20:43:44 +0530

    first commit

---
+++ src/Imagess.js
@@ -0,0 +1,21 @@
+function Imagess() {
+    return (
+        <div className="imgs">
+            <div class="image-container">
+
+                <img src="https://i.pinimg.com/736x/5d/c1/5f/5dc15f1e7679bf50f2832a76daddd3b2.jpg" alt="Image 1" className="img1" />
+                <div className="tx1">STRENGTH</div>
+            </div>
+            <div class="image-container">
+                <img src="https://i.pinimg.com/736x/a6/7f/17/a67f170bbcdc44bf1b7722684b215aa5.jpg" alt="Image 2" />
+                <div className="tx1">MOBILITY</div>
+            </div>
+            <div class="image-container">
+                <img src="https://i.pinimg.com/736x/98/a8/3a/98a83a3979c33bfb19d911a6a2b91242.jpg" alt="Image 3" className="img3" />
+                <div className="tx1">DRILLS </div>
+            </div>
+
+        </div>
+    )
+}
+export default Imagess;
